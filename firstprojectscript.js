43854bec45b5e67296843abfeaeb23f0a9e57b26
Cassie Malatak <cjmalatak@gmail.com>
2019-01-22 15:50:57 -0500

    update input validation

--- firstprojectscript.js
+++ firstprojectscript.js
@@ -25,8 +25,10 @@
         for (let k=0; k<input; k++){
             input[k].checked === true;
             if (true) {
-                console.log('This is the answer');
-            } //else k++ here instead?
+                console.log(`${input[k]} is the answer to ${question[j]}`);
+                //Add an indicator depending on which input is the true one...
+                // const answerValue[j] = k ?
+            }
         }
     }
 
@@ -43,30 +45,18 @@
 
 
     const perfectPet = () => {
-        const question1Answer = () => {
-            // if inputResult0,1,2,3, (or prompt?) checked === true, then value = the number value of the input 
-            //question1 = value of checked input
-        }
-        const question2Answer = () => {
-            // if inputResult4,5,6,7, (or prompt?) checked === true, then value = the number value of the input 
-            //question2 = value of checked input
-        }
-        const question3Answer = () => {
-            // if inputResult8,9,10,11, (or prompt?) checked === true, then value = the number value of the input 
-            //question3 = value of checked input
-        }
-        const finalScore = (x+y+z);
-            const x = parseInt(question1Answer);
-            const y = parseInt(question2Answer);
-            const z = parseInt(question3Answer);
-        if (parseInt(finalScore) <= 16) {
-            let perfectPet = "Cat!"
-            console.log('Your perfect pet is a...')
-            console.log(perfectPet)
-        } else {
-            let perfectPet = "Dog!"
-            console.log('Your perfect pet is a...')
-            console.log(perfectPet)
-        }
-    }
+    //     const finalScore = (answerValues);
+    //         const x = parseInt(question1Answer);
+    //         const y = parseInt(question2Answer);
+    //         const z = parseInt(question3Answer);
+    //     if (parseInt(finalScore) <= 16) {
+    //         let perfectPet = "Cat!"
+    //         console.log('Your perfect pet is a...')
+    //         console.log(perfectPet)
+    //     } else {
+    //         let perfectPet = "Dog!"
+    //         console.log('Your perfect pet is a...')
+    //         console.log(perfectPet)
+    //     }
+    // }
 }
